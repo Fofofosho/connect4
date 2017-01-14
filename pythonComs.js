795b544aef0dd4609cf3261e7ee4c01ce78d90cf
Forrest Townsend <forrest.town18@gmail.com>
2017-01-14 18:23:43 +0000

    save more status

--- pythonComs.js
+++ pythonComs.js
@@ -1,7 +1,13 @@
-//rabbitMQ to talk to Dan's end?
+//rabbitMQ to talk to Dan's end
 //https://www.rabbitmq.com/tutorials/tutorial-one-javascript.html
+const amqp = require('amqplib/callback_api');
+
+
+function sendInfo (packet) {
+    console.log(packet);
+}
+
+
 module.exports = {
-    sendInfo : function (packet) {
-        console.log(packet);
-    },
+    sendInfo : sendInfo,
 };
