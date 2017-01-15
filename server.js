79e5848d5a18b277a2b8b940ac1577e79c9e736d
Forrest Townsend <forrest.town18@gmail.com>
2017-01-15 13:12:25 +0000

    Potentially a better img refresh

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var io = require('socket.io')(http);
 // Amazon specific declarations
 const AWS = require("aws-sdk");
 AWS.config.loadFromPath('./awsconfig.json');
-const s3 = new AWS.S3();
 const sqs = new AWS.SQS();
 const objectBucketName = 'connect4-hackaz17';
 
@@ -26,22 +25,23 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// app.post('/', function(req, res) {
-// });
-
-app.post('/imgData', function(req, res) {
-    let fs = require("fs");
-    //update the local static image, *cheaper probably than s3*
-});
-
+let timerArray = [];
 app.post('/gameData', function(req, res) {
     //Get the data Dan's detector sent for the game data
     console.log(req);
     console.log(req.body);
     let dansBody = req.body;
+    timerArray.push(dansBody);
+    
     io.emit('dan', {
         "arrayInfo": dansBody
     });
+    
+    setTimeout(function() {
+        io.emit("picUpdate", {
+            "objectName": `https://s3-us-west-2.amazonaws.com/${objectBucketName}/poop.png`
+        });
+    }, 1000);
     
     res.end();
 });
@@ -52,34 +52,8 @@ io.on('connection', function(socket) {
         "objectName": `https://s3-us-west-2.amazonaws.com/${objectBucketName}/poop.png`
     });
     
-    // setInterval(readIncomingMessages(), 20000);
     readIncomingMessages();
 });
-
-function checkDate() {
-    console.log("checkDate:");
-    //could do this on the server portion too
-    let lastCheck = new Date();
-    setInterval(function() {
-        console.log("Checking s3");
-        let params = {
-            "Bucket": objectBucketName, /* required */
-            "Key": 'poop.png', /* required */
-            "IfModifiedSince": lastCheck,
-        }
-        s3.headObject(params, function(err, data) {
-            if(err) {
-                console.log("ERROR with GET " + err);
-            }
-            else {
-                lastCheck = new Date();
-                io.emit("picUpdate", {
-                    objectName: `https://s3-us-west-2.amazonaws.com/${objectBucketName}/${params.key}`
-                });
-            }
-        });
-    }, 5000);
-}
 
 function readIncomingMessages() {
     let getParams = {
