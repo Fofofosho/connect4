858b2b754c7ff658bafb23a9e60a9b0917d2eacb
Forrest Townsend <forrest.town18@gmail.com>
2017-01-15 13:33:40 +0000

    Added checking for win

--- public/js/Grid.js
+++ public/js/Grid.js
@@ -75,27 +75,39 @@
                 window.Game.currentPlayer = window.Game.getCurrentPlayer(obj);
             }
         }
-        //this.checkForVictory();
+        console.log(this.chkWinner(this.get2dArray()));
     }
-    container.checkForVictory = function(row,col){
-        if(this.getAdj(row,col,0,1)+this.getAdj(row,col,0,-1) > 2){ return true; } 
-        else {
-            if(this.getAdj(row,col,1,0) > 2){ return true; } 
-            else {
-                if(this.getAdj(row,col,-1,1)+this.getAdj(row,col,1,-1) > 2){ return true; } 
-                else {
-                    if(this.getAdj(row,col,1,1)+this.getAdj(row,col,-1,-1) > 2){ return true; } 
-                    else { return false; }
-                }
-            }
-        }
+    container.chkLine = function(a,b,c,d) {
+        // Check first cell non-zero and all cells match
+        return ((a != 0) && (a ==b) && (a == c) && (a == d));
     }
-    container.getAdj = function(row,col,row_inc,col_inc){
-        if(this.cellVal(row,col) == this.cellVal(row+row_inc,col+col_inc)){ return 1+this.getAdj(row+row_inc,col+col_inc,row_inc,col_inc); } 
-        else { return 0; }
-    }
-    container.cellVal = function(row,col){
-        return this.get2dArray()[row][col];
+    
+    container.chkWinner = function(bd) {
+        // Check down
+        for (r = 0; r < 3; r++)
+            for (c = 0; c < 7; c++)
+                if (chkLine(bd[r][c], bd[r+1][c], bd[r+2][c], bd[r+3][c]))
+                    return bd[r][c];
+    
+        // Check right
+        for (r = 0; r < 6; r++)
+            for (c = 0; c < 4; c++)
+                if (chkLine(bd[r][c], bd[r][c+1], bd[r][c+2], bd[r][c+3]))
+                    return bd[r][c];
+    
+        // Check down-right
+        for (r = 0; r < 3; r++)
+            for (c = 0; c < 4; c++)
+                if (chkLine(bd[r][c], bd[r+1][c+1], bd[r+2][c+2], bd[r+3][c+3]))
+                    return bd[r][c];
+    
+        // Check down-left
+        for (r = 3; r < 6; r++)
+            for (c = 0; c < 4; c++)
+                if (chkLine(bd[r][c], bd[r-1][c+1], bd[r-2][c+2], bd[r-3][c+3]))
+                    return bd[r][c];
+    
+        return 0;
     }
 
     window.Grid = createjs.promote(Grid, "Container");
