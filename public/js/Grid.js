2de9b7f22ed63e875a4f8f51126679ae8d1235fe
Forrest Townsend <forrest.town18@gmail.com>
2017-01-15 13:57:54 +0000

    the blessings

--- public/js/Grid.js
+++ public/js/Grid.js
@@ -84,28 +84,28 @@
     
     container.chkWinner = function(bd) {
         // Check down
-        for (r = 0; r < 3; r++)
-            for (c = 0; c < 7; c++)
-                if (this.chkLine(bd[r][c], bd[r+1][c], bd[r+2][c], bd[r+3][c]))
-                    return bd[r][c];
+        for (var r = 0; r < 3; r++)
+            for (var c = 0; c < 7; c++)
+                if (this.chkLine(bd[r][c].type, bd[r+1][c].type, bd[r+2][c].type, bd[r+3][c].type))
+                    return bd[r][c].type;
     
         // Check right
-        for (r = 0; r < 6; r++)
-            for (c = 0; c < 4; c++)
-                if (this.chkLine(bd[r][c], bd[r][c+1], bd[r][c+2], bd[r][c+3]))
-                    return bd[r][c];
+        for (var r = 0; r < 6; r++)
+            for (var c = 0; c < 4; c++)
+                if (this.chkLine(bd[r][c].type, bd[r][c+1].type, bd[r][c+2].type, bd[r][c+3].type))
+                    return bd[r][c].type;
     
         // Check down-right
-        for (r = 0; r < 3; r++)
-            for (c = 0; c < 4; c++)
-                if (this.chkLine(bd[r][c], bd[r+1][c+1], bd[r+2][c+2], bd[r+3][c+3]))
-                    return bd[r][c];
+        for (var r = 0; r < 3; r++)
+            for (var c = 0; c < 4; c++)
+                if (this.chkLine(bd[r][c].type, bd[r+1][c+1].type, bd[r+2][c+2].type, bd[r+3][c+3].type))
+                    return bd[r][c].type;
     
         // Check down-left
-        for (r = 3; r < 6; r++)
-            for (c = 0; c < 4; c++)
-                if (this.chkLine(bd[r][c], bd[r-1][c+1], bd[r-2][c+2], bd[r-3][c+3]))
-                    return bd[r][c];
+        for (var r = 3; r < 6; r++)
+            for (var c = 0; c < 4; c++)
+                if (this.chkLine(bd[r][c].type, bd[r-1][c+1].type, bd[r-2][c+2].type, bd[r-3][c+3].type))
+                    return bd[r][c].type;
     
         return 0;
     }
